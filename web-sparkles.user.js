3f752102d00123df741a0a660f75cdc27997069f
Yeosangist <icequeenofeverything@gmail.com>
2026-09-18 10:33:25 +1200

    Change update and download URLs in user script

--- web-sparkles.user.js
+++ web-sparkles.user.js
@@ -5,8 +5,8 @@
 // @description  Adds animated sparkle effects to any webpage
 // @author       Yeosangist
 // @match        *://*/*
-// @updateURL    https://github.com/Yeosangist/desktopSparkles/blob/main/web-sparkles.user.js
-// @downloadURL  https://github.com/Yeosangist/desktopSparkles/blob/main/web-sparkles.user.js
+// @updateURL    https://github.com/Yeosangist/sparkleSuite/raw/refs/heads/main/web-sparkles.user.js
+// @downloadURL  https://github.com/Yeosangist/sparkleSuite/raw/refs/heads/main/web-sparkles.user.js
 // @license      CC BY-NC-SA 4.0
 // @grant        none
 // ==/UserScript==
